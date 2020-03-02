2e547a3f3573354723bf436fb48036775881da7f
David_Calderón <jd218@misena.edu.co>
2020-03-02 14:15:05 -0500

    Guardando Ejercicio Atributos

---
+++ Atributos/Atributos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aprendiz apSena = new Aprendiz();
+        apSena.Nombres = "Jesus David";
+        apSena.Apellidos = "Calderón Gomez";
+        Saludo(apSena);
+    }
+    private static void Saludo(Aprendiz ApSena)
+    {
+        Console.WriteLine("Bienvenido, sr(a) " 
+            + ApSena.Nombres);
+        Console.ReadLine();
+    }
+}
+
+
--- FunconConRetornoConParametros/FunconConRetornoConParametros/Program.cs
+++ FunconConRetornoConParametros/FunconConRetornoConParametros/Program.cs
@@ -1,14 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace FunconConRetornoConParametros
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int NumeroNotas = 0;
+        Console.WriteLine("===NOTAS===");
+        Console.WriteLine("Ingrese el número de notas: ");
+        Int32.TryParse(Console.ReadLine(), out NumeroNotas);
+        string Resultado = CalcularNotas(NumeroNotas);
+    }
+    public static string CalcularNotas(int NumeroNotas)
+    {
+        decimal Nota = 0;
+        decimal SumaNotas = 0;
+        for (int i = 1; i <= NumeroNotas; i++)
         {
+            Console.Write("Ingrese la nota N. " + i);
+            decimal.TryParse(Console.ReadLine(), out Nota);
+            //...
         }
+        //Calcular la nota definitiva y devolver el mensaje
+        //si aprobó o no aprobó
+        return "";
     }
 }
+
