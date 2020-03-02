dfb6d502a97b9143e6c7caecd5b0cf4e6b405363
JDavid80 <34772030+JDavid80@users.noreply.github.com>
2020-03-02 15:39:33 -0500

    Editando Archivo Hola Mundo

--- HolaMundo/HolaMundo/Program.cs
+++ HolaMundo/HolaMundo/Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hola Mundo!");
+        Console.WriteLine("Esta es una prueba de Control de Versiones!");
         Console.ReadLine();
     }
 }
